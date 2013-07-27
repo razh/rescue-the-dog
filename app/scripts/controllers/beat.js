7cdcf8fbe8826d583088636d0d63d845839212bb
Raymond Zhou <somecrazyidiot@gmail.com>
2013-07-27 02:33:11 -0400

    Add some keyboard shortcuts for navigating between beats.

--- app/scripts/controllers/beat.js
+++ app/scripts/controllers/beat.js
@@ -3,9 +3,11 @@
 angular.module( 'rescueTheDogApp' )
   .controller( 'BeatCtrl', function( $scope, $routeParams ) {
     var beatId = $routeParams.beatId;
+
     for ( var i = 0, l = $scope.beats.length; i < l; i++ ) {
       if ( $scope.beats[i].id === beatId ) {
         $scope.selected.beat = $scope.beats[i];
+        $scope.selected.index = i;
         break;
       }
     }
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,23 +1,75 @@
 'use strict';
 
 angular.module( 'rescueTheDogApp' )
-  .controller( 'MainCtrl', function( $scope, $http ) {
+  .controller( 'MainCtrl', function( $scope, $http, $window, $location ) {
     $scope.selected = {
-      beat: null
+      beat: null,
+      index: null
     };
 
     $scope.text = {};
 
-    $scope.getBeats = function() {
-      return $http.get( './json/beats.json' )
-        .success(function( data ) {
-          $scope.beats = data;
+    // Grab beat title and description data.
+    $http.get( './json/beats.json' )
+      .success(function( data ) {
+        $scope.beats = data;
 
-          $scope.beats.forEach(function( beat ) {
-            $scope.text[ beat.id ] = '';
-          });
+        $scope.beats.forEach(function( beat ) {
+          $scope.text[ beat.id ] = '';
         });
+      });
+
+    $scope.isSelected = function( index ) {
+      return index === $scope.selected.index;
     };
 
-    $scope.getBeats();
+    // Handle jumping from beat to beat with key commands.
+    $scope.jumpId = function( distance ) {
+      var length = $scope.beats.length,
+          index = $scope.selected.index || 0,
+          newIndex = Math.min( Math.max( 0, index + distance ), length - 1 );
+
+      return $scope.beats[ newIndex ].id;
+    };
+
+    $scope.prevId = function() {
+      return $scope.jumpId(-1);
+    };
+
+    $scope.nextId = function() {
+      return $scope.jumpId(1);
+    };
+
+    function onKeyDown( event ) {
+      if ( event.ctrlKey ) {
+        if ( event.which === 38 ||
+             event.which === 40 ) {
+          event.preventDefault();
+
+          // CTRL + UP.
+          if ( event.which === 38 ) {
+            $location.path( '/beat/' + $scope.prevId() );
+          }
+          // CTRL + DOWN.
+          else if ( event.which === 40 ) {
+            $location.path( '/beat/' + $scope.nextId() );
+          }
+
+          $scope.$apply();
+        }
+      }
+
+      // ESC.
+      if ( event.which === 27 ) {
+        event.preventDefault();
+
+        // Since we're back at home, we don't have an index selected.
+        $scope.selected.index = null;
+
+        $location.path( '/' );
+        $scope.$apply();
+      }
+    }
+
+    angular.element( $window ).bind( 'keydown', onKeyDown );
   });
